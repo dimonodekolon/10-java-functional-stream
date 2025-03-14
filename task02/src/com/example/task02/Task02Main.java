f693fc757528bca1453dc270de3224b10fbcbad4
dimonodekolon <daluch@mail.ru>
2025-03-14 20:44:11 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,21 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException("n must be between 1 and 16");
+        }
 
-        return null; // your implementation here
+        int max = 1 << n; // 2^n
 
+        return IntStream.iterate(0, x -> (x + 1) % max) // Генерируем числа от 0 до 2^n - 1, циклично
+                .map(x -> x ^ (x >> 1)); // Преобразуем число в код Грея
     }
 
 }
